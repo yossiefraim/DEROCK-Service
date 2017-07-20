8321fe01fcc6ba43b8c67c99c085e28d9d6c1380
yossiefraim <yossiefraim0@gmail.com>
2017-07-20 14:50:12 +0300

    commit

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -80,7 +80,7 @@ exports.login = ((user_id)=>{
             });
       });
       let playlistData = new Promise((resolve,reject)=>{
-              playlists.find({{}}).exec(function(err,result){
+              playlists.find({}).exec(function(err,result){
                 if(!err){
                   if(result==null){
                     reject('error: ${err}');
